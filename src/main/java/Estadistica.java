f5b411bdb2ab5083b6ccc22a4985629a3aa9d253
al364113 <al364113@uji.es>
2022-01-25 13:36:56 +0100

    Inicio

---
+++ src/main/java/Estadistica.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Estadistica {
+
+    public double mediaAritmetica(ArrayList<Integer> vector){
+        int suma = 0;
+        int longitud = vector.size();
+        for(int numero: vector){
+            suma += numero;
+        }
+        double media = suma / longitud;
+
+        return media;
+    }
+
+    public int varianza(ArrayList<Integer> vector){
+
+    }
+
+    public int desviacionEstandar(ArrayList<Integer> vector){
+
+    }
+}
---
+++ src/test/java/EstadisticaTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstadisticaTest {
+
+    @org.junit.jupiter.api.Test
+    void mediaAritmetica() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void varianza() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void desviacionEstandar() {
+    }
+}
