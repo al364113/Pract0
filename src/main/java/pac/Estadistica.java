788f2c3671bcd91340ef965ed1e31a45dfff5dc2
al364113 <al364113@uji.es>
2022-01-26 10:08:38 +0100

    Fórumlas corregidas. Test creados.

---
+++ src/main/java/pac/Estadistica.java
@@ -0,0 +1,43 @@
+package pac;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class Estadistica {
+
+
+    public static double mediaAritmetica(ArrayList<Integer> vector) {
+        double suma = 0;
+        double longitud = vector.size();
+        for (int numero : vector) {
+            suma += numero;
+        }
+        double media = suma / longitud;
+
+        return media;
+    }
+
+    public static double varianza(ArrayList<Integer> vector) {
+        double media = mediaAritmetica(vector);
+        double sumatorio = 0;
+        for (int numero : vector) {
+            sumatorio += Math.pow(numero - media, 2);
+        }
+        double varianza = sumatorio / vector.size();
+        return varianza;
+    }
+
+    public static double desviacionEstandar(ArrayList<Integer> vector) {
+        /*int longitud = vector.size();
+        double media = mediaAritmetica(vector), sumatorio = 0, s = 0;
+        for (int numero : vector){
+            sumatorio += Math.pow((numero - media), 2);
+        }
+        s = Math.sqrt(sumatorio / (longitud - 1));*/
+        double varianza = varianza(vector);
+        double desviacionTipica = Math.sqrt(varianza);
+        double redondeo = Math.round(desviacionTipica * 100.0) / 100.0; //Redondeo a dos decimales
+        return redondeo;
+
+    }
+}
---
+++ src/test/java/pac/EstadisticaTest.java
@@ -0,0 +1,69 @@
+package pac;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class EstadisticaTest {
+
+    @Test
+    void mediaAritmetica() {
+        ArrayList<Integer> vect = new ArrayList<Integer>();
+        vect.add(5); vect.add(5); vect.add(5); vect.add(5);
+        assertEquals(Estadistica.mediaAritmetica(vect), 5);
+
+        vect.clear();
+        vect.add(1); vect.add(2); vect.add(3); vect.add(4);
+        assertEquals(Estadistica.mediaAritmetica(vect), 2.5);
+
+        vect.clear();
+        vect.add(1);
+        assertEquals(Estadistica.mediaAritmetica(vect), 1);
+
+        vect.clear();
+        vect.add(1); vect.add(-1);
+        assertEquals(Estadistica.mediaAritmetica(vect), 0);
+
+        vect.clear();
+        vect.add(-1);
+        assertEquals(Estadistica.mediaAritmetica(vect), -1);
+
+    }
+
+    @Test
+    void varianza() {
+        ArrayList<Integer> vect = new ArrayList<Integer>();
+        vect.add(12); vect.add(6); vect.add(7); vect.add(3); vect.add(15); vect.add(10); vect.add(18); vect.add(5);
+        assertEquals(Estadistica.varianza(vect), 23.75);
+
+        vect.clear();
+        vect.add(5); vect.add(5); vect.add(12); vect.add(13); vect.add(15); vect.add(15); vect.add(15); vect.add(20); vect.add(20); vect.add(23);
+        assertEquals(Estadistica.varianza(vect), 32.21);
+
+        vect.clear();
+        vect.add(150); vect.add(160); vect.add(164); vect.add(158); vect.add(183);
+        assertEquals(Estadistica.varianza(vect), 120.8);
+
+    }
+
+    @Test
+    void desviacionEstandar() {
+        ArrayList<Integer> vect = new ArrayList<Integer>();
+        vect.add(9); vect.add(3); vect.add(8); vect.add(8); vect.add(9); vect.add(8); vect.add(9); vect.add(18);
+        assertEquals(Estadistica.desviacionEstandar(vect),3.87);
+
+        vect.clear();
+        vect.add(2); vect.add(4); vect.add(3);
+        assertEquals(Estadistica.desviacionEstandar(vect),0.82);
+
+        vect.clear();
+        vect.add(2); vect.add(1); vect.add(3); vect.add(2);
+        assertEquals(Estadistica.desviacionEstandar(vect),0.71);
+
+        vect.clear();
+        vect.add(7); vect.add(5); vect.add(6); vect.add(8); vect.add(7); vect.add(8); vect.add(8); vect.add(9); vect.add(10); vect.add(10);
+        assertEquals(Estadistica.desviacionEstandar(vect),1.54);
+    }
+}
